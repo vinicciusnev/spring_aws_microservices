7008154447e02a809adf42d4b1e2267b3bcf59e8
vinicciusnev <xxnashflows@hotmail.com>
2025-09-18 21:14:00 -0300

    feat(dto): Implementando dto's de request e response para Product

---
+++ src/main/java/com/viniccius/aws/spring_aws_microservices/dto/response/ProductResponseDTO.java
@@ -0,0 +1,9 @@
+package com.viniccius.aws.spring_aws_microservices.dto.response;
+
+public record ProductResponseDTO(
+        Long id,
+        String name,
+        String model,
+        String code,
+        float price
+) {}
